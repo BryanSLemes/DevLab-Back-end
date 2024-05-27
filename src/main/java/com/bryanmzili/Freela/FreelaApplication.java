0d954823008c7eeb1e3ea193bf293a286d52b329
BryanMzili <= bryansilvalemes@gmail.com>
2024-05-27 11:36:31 -0300

    Estrutura do projeto

---
+++ src/main/java/com/bryanmzili/Freela/FreelaApplication.java
@@ -0,0 +1,13 @@
+package com.bryanmzili.Freela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bryanmzili/Freela/FreelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryanmzili.Freela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
