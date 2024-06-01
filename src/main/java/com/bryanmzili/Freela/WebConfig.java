9bd78443725f527cc808819cfc5112ae8a9a93e0
BryanSLemes <bryansilvalemes@gmail.com>
2024-05-31 22:57:51 -0300

    Sessões definidas com token + proteção de rotas

---
+++ src/main/java/com/bryanmzili/Freela/WebConfig.java
@@ -0,0 +1,18 @@
+package com.bryanmzili.Freela;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true);
+    }
+
+}
